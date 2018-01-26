c2eac725a7395d57dcb94ef6e071f3eaf92b29ec
fahrul98 <fahrul.gh@gmail.com>
2018-01-26 11:45:14 +0700

    2

--- application/views/core/navbar.php
+++ application/views/core/navbar.php
@@ -9,6 +9,7 @@
   //jika mode di controller ada, maka muncul navbar utk pengunjung
   if (isset($mode)&&$mode=="pengunjung") {
 ?>
+<<<<<<< HEAD
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="<?php echo base_url('');?>">[][][]Situs[][][]</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,6 +44,39 @@
     </span>
   </div>
 </nav>
+=======
+<div class="container">
+<div class="nav-side-menu">
+    <div class="brand"><a href="#Profile" style="text-decoration: none; color: #e1ffff;">CMS Masjid</a></div>
+      <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+        <div class="menu-list">
+            <ul id="menu-content" class="menu-content collapse out">            
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('pengunjung/post')?>"><i class="fa fa-pencil fa-lg"></i> Post </span></a>
+              </li>  
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('pengunjung/profilm')?>"><i class="fa fa-id-card-o fa-lg" aria-hidden="true"></i> Profil m </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('pengunjung/takmirm')?>"><i class="fa fa-pencil fa-lg"></i> Takmir m </span></a>
+              </li>  
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('pengunjung/ustadz')?>"><i class="fa fa-pencil fa-lg"></i> Daftar Ustadz  <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('pengunjung/keuanganmasjid')?>"><i class="fa fa-usd fa-lg"></i>Keuangan Masjid <span class="sr-only">(current)</span></a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('pengunjung/jadwalkegiatan')?>"><i class="fa fa-calendar-check-o fa-lg"></i> Jadwal Kegiatan  <span class="sr-only">(current)</span></a>
+              </li>
+            </ul>
+            <span class="navbar-text">
+             <?php echo md5('pengunjung') ?>
+            </span>
+     </div>
+</div>
+</div>
+>>>>>>> 1ccb56942216d0120ba8f442ed3d964c6fd11770
 <?php
 
 //jika tidak maka muncul punya si admin
@@ -51,55 +85,55 @@
 
 ?>
 <!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="<?php echo base_url('pengunjung');?>">[][][]Situs[][][]</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo base_url('admin')?>">admin <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('post')?>">Post</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('profiladmin')?>">Profil Admin</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('profilm')?>">Profil Masjid</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('takmir')?>">Takmir</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('ustadz')?>">Ustadz</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('media')?>">Media</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('jadwalkegiatan')?>">Jadwal Kegiatan</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('keuanganmasjid')?>">Keuangan Masjid</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('rekamdonasi')?>">R. Donasi</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('pengaturan')?>">Pengaturan</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="<?php echo base_url('admin/logout')?>">Logout</a>
-      </li>
-    </ul>
+<div class="container">
+<div class="nav-side-menu">
+    <div class="brand"><a href="#Profile" style="text-decoration: none; color: #e1ffff;">CMS Masjid</a></div>
+      <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
+        <div class="menu-list">
+            <ul id="menu-content" class="menu-content collapse out">
+              <li  class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('admin')?>"><i class="fa fa-user-o fa-lg"></i><span class="sr-only">(current)</span> admin</a>
+              </li>               
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('post')?>"><i class="fa fa-pencil fa-lg"></i> Post </span></a>
+              </li>  
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('profiladmin')?>"><i class="fa fa-id-card-o fa-lg" aria-hidden="true"></i>    Profil Admin</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('profilm')?>"><i class="fa fa-list-alt fa-lg"></i> Profil Masjid </span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('takmir')?>"><i class="fa fa-pencil fa-lg"></i> Takmir </span></a>
+              </li>  
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('ustadz')?>"><i class="fa fa-pencil fa-lg"></i> Ustadz </span></a>
+              </li> 
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('media')?>"><i class="fa fa-calendar-check-o fa-lg"></i> Media </span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('jadwalkegiatan')?>"><i class="fa fa-calendar-check-o fa-lg"></i> Jadwal Kegiatan </span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('keuanganmasjid')?>"><i class="fa fa-usd fa-lg"></i>Keuangan Masjid</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url('rekamdonasi')?>"><i class="fa fa-usd fa-lg"></i> Rekam Donasi </a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="<?php echo base_url('pengaturan')?>""><i class="fa fa-cog fa-lg"></i> Pengaturan</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="<?php echo base_url('admin/logout')?>""><i class="fa fa-cog fa-lg"></i> Logout</a>
+              </li>
+            </ul>
     <span class="navbar-text">
       <?php echo md5('dib') ?>
     </span>
-  </div>
-</nav>
+     </div>
+</div>
+</div>
 <?php
 }
 ?>
--- application/views/vlogin.php
+++ application/views/vlogin.php
@@ -6,9 +6,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="container">
 	<!-- <h1>Welcome to CodeIgniter!</h1> -->
 	<h2><?php echo $page; ?></h2>
-	</div>
+</div>
 	<?php echo form_open('login/dblogin');?>
+<<<<<<< HEAD
 	<label for="username">Username</label><input type="text" name="username" value="admin">
 	<label for="userpass">Password</label><input type="textarea" name="userpass" value="pass">
+=======
+<div class="container" style="margin-left: 400px; margin-top: 150px;">
+    	  <div class="modal-dialog">
+				<div class="loginmodal-container">
+					<h1>Login</h1><br>
+				  <form>
+					<input type="text" name="username" value="admin" placeholder="Username">
+					<input type="password" name="userpass" value ="passsecret" placeholder="Password">
+					<input type="submit" name="login" class="login loginmodal-submit" value="login">
+				  </form>
+					
+				  <div class="login-help">
+					<a href="#">Register</a> - <a href="#">Forgot Password</a>
+				  </div>
+				</div>
+			</div>
+</div>
+<!-- 	<label for="username">Username</label><input type="text" name="username" value="admin">
+	<label for="userpass">Password</label><input type="textarea" name="userpass" value="passsecret">
+>>>>>>> 1ccb56942216d0120ba8f442ed3d964c6fd11770
 	<label for="capt">Captcha</label><input type="textarea" name="capt" value="">
 	<input type="submit" name="login" value="login">
+ -->
