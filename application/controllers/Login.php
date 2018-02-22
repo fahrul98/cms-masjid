384c07dd46a376604efaef19074e356f59b48579
fahrul98 <fahrul.gh@gmail.com>
2018-02-22 19:24:29 +0700

    lupa dan ubah password
    +form lupad dan ubah
    +link token
    +tabel token
    -validasi form
    -kirim email

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -52,4 +52,82 @@ class Login extends CI_Controller {
 			redirect(base_url('login'));
 		}
 	}
+
+	public function forget(){
+		$data['page'] = "Forget Password";
+		if ($this->session->userdata('username') and $this->session->userdata('userpass')){
+			redirect(base_url('admin'));
+		}else{
+			$this->load->view('core/core',$data);
+			$this->load->view('vlogin');
+		}
+	}
+
+	public function dbsendemail(){
+		$data['email'] = $this->input->post('email');   
+        $clean= $this->security->xss_clean($data['email']);  
+        $userInfo = $this->mprofiladmin->getUserInfoByEmail($clean);
+        if(!$userInfo){ 
+        	//echo "error";
+        	redirect(base_url('login/forget/'));
+        }
+
+        $token = $this->mprofiladmin->insertToken($userInfo->userid);    
+        $url = base_url().'login/reset_password/'.$token;  
+        $link = '<a href="' . $url . '">' . $url . '</a>';   
+               
+        $message = '';             
+        $message .= '<strong>Hai, anda menerima email ini karena ada permintaan untuk memperbaharui password anda.</strong><br>';  
+        $message .= '<strong>Silakan klik link ini:</strong> ' . $link;  
+        echo $message;       
+
+//error kirim email
+		// $config = array(
+  //       'protocol' => 'smtp',
+  //       'smtp_host' => 'ssl://smtp.gmail.com',
+  //       'smtp_port' =>  465, 
+  //       'smtp_user' => '2luck4nut@gmail.com',
+  //       'smtp_pass' => 'nutcracker',
+  //       // 'smtp_crypto' => 'tls',
+  //       'smtp_timeout' => '30',
+  //       'mailtype'  => 'html', 
+  //       'charset'   => 'utf-8',
+  //       'newline'	=>	"\r\n"
+  //   	);
+		// $this->load->library('email', $config);
+		// $this->email->to($clean);
+		// $this->email->from('2luck4nut@gmail.com','luck nut');
+		// $this->email->subject('link ubah password (HTML)');
+		// $this->email->message($message);
+		// $result=$this->email->send();
+		// var_dump($result);
+
+		// $data['page'] = "Email Terkirim";
+		// $this->load->view('core/core',$data);
+		// $this->load->view('vlogin');
+	}
+
+	public function reset_password(){
+		$token = $this->uri->segment(3);           
+    	$cleanToken = $this->security->xss_clean($token);  
+        $data['user'] = $this->mprofiladmin->isTokenValid($cleanToken);         
+         
+		if(!$data['user']){ 
+			//echo $cleanToken." ok";
+		 	redirect(base_url('login'));   
+		}
+
+		$data['page'] = "Change Password";
+		$this->load->view('core/core',$data);
+		$this->load->view('vlogin');
+	}
+
+	public function dbchangepass(){
+		$data['userpass'] = $this->input->post('newpass');
+		$data['userid'] = $this->input->post('userid');
+		$q= $this->mprofiladmin->updatePassword($data);
+		if ($q) {
+			redirect(base_url('login'));
+		}
+	}
 }
--- application/models/Mprofiladmin.php
+++ application/models/Mprofiladmin.php
@@ -69,4 +69,80 @@ input->proses->hapus dari memori
 		unset($data);
 	}
 
+	//Start: method tambahan untuk reset code  
+   public function getUserInfo($id)  
+   {  
+     $q = $this->db->get_where('cmusers', array('userid' => $id), 1);   
+     if($this->db->affected_rows() > 0){  
+       $row = $q->row();  
+       return $row;  
+     }else{  
+       error_log('no user found getUserInfo('.$id.')');  
+       return false;  
+     }  
+   }  
+   
+  public function getUserInfoByEmail($email){  
+     $q = $this->db->get_where('cmusers', array('useremail' => $email), 1);   
+     if($this->db->affected_rows() > 0){  
+       $row = $q->row();  
+       return $row;  
+     } 
+      unset($data,$q,$row);
+   }  
+   
+   public function insertToken($user_id)  
+   {    
+     $token = substr(sha1(rand()), 0, 30);   
+     $date = date('Y-m-d');  
+       
+     $string = array(  
+         'token'=> $token,  
+         'userid'=>$user_id,  
+         'created'=>$date  
+       );  
+     $query = $this->db->insert_string('cmtokens',$string);  
+     $this->db->query($query);  
+     return $token . $user_id;  
+       
+   }  
+   
+   public function isTokenValid($token)  
+   {  
+     $tkn = substr($token,0,30);  
+     $uid = substr($token,30);     
+       
+     $q = $this->db->get_where('cmtokens', array(  
+       'cmtokens.token' => $tkn,   
+       'cmtokens.userid' => $uid), 1);               
+           
+     if($this->db->affected_rows() > 0){  
+       $row = $q->row();         
+         
+       $created = $row->created;  
+       $createdTS = strtotime($created);  
+       $today = date('Y-m-d');   
+       $todayTS = strtotime($today);  
+         
+       if($createdTS != $todayTS){  
+         return false;  
+       }  
+         
+       $user_info = $this->getUserInfo($row->userid);  
+       return $user_info;  
+         
+     }else{  
+       return false;  
+     }  
+       
+   }   
+   
+   public function updatePassword($post)  
+   {    
+     $this->db->where('userid', $post['userid']);  
+     $this->db->update('cmusers', array('userpass' => md5($post['userpass'])));      
+     return true;  
+   }   
+   //End: method tambahan untuk reset code  
+
 }
